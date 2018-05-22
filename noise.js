816d680a31099cfb4517a42d27995aa8dc1deaf1
Abiaina <abinnet.a@gmail.com>
2018-05-21 22:40:29 -0700

    changed css and html classes, each button can play 1 note once

--- noise.js
+++ noise.js
@@ -1,3 +1,30 @@
-$(document).ready( function() {
-  // your code here
+// clicks on sound
+$(document).ready(function() {
+  $('button.noteC').click(function(){
+    $('#cAudio')[0].play();
+  });
+
+  $('button.noteD').click(function(){
+    $('#dAudio')[0].play();
+  });
+
+  $('button.noteE').click(function(){
+    $('#eAudio')[0].play();
+  });
+
+  $('button.noteF').click(function(){
+    $('#fAudio')[0].play();
+  });
+
+  $('button.noteG').click(function(){
+    $('#gAudio')[0].play();
+  });
+
+  $('button.noteA').click(function(){
+    $('#aAudio')[0].play();
+  });
+
+  $('button.noteB').click(function(){
+    $('#bAudio')[0].play();
+  });
 });
